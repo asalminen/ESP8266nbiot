4b80e1049ac7debd6d184c53827b94e8631f7aa4
Aki Salminen <code@salminen.info>
2018-11-27 13:09:47 +0200

    Add timeout for AT send and poweron for D1

--- ESP8266nbiot.ino
+++ ESP8266nbiot.ino
@@ -3,7 +3,7 @@
 // Connect modem  tx to ESP D7 (rx)
 //                rx to ESP D8 (tx)
 //                G to G and V to V3,3
-//                Connect modem K to modem other G with jumpper cable ( on key pressed )
+//                Connect modem K ESP D1 ( on key pressed )
 
 int8_t pollModem = 1;
 uint32 millisTime = 0;
@@ -15,9 +15,6 @@ void setup() {
   modem.begin(115200);
 
   pinMode(D1, OUTPUT); // Modem power key
-  digitalWrite( D1, LOW ); // Start modem
-  delay(200);
-  //  digitalWrite( D1, HIGH );
   Serial.println("NB-IOT MQTT test");
   setupConnection();
 }
@@ -44,6 +41,7 @@ void loop() {
 }
 
 void setupConnection() {
+  PowerOnModem();
   //char *ATCommand;
   //ATCommand="ATZ";
   pollModem = 0;
@@ -57,10 +55,12 @@ void setupConnection() {
     Serial.println("Modeemin SIM kortti ei toimi.");
     reboot();
   }
+
   sendATCommand((char *)"AT+CSQ");
   sendATCommand((char *)"AT+CGREG?");
   sendATCommand((char *)"AT+CGACT?");
   sendATCommand((char *)"AT+COPS?");
+
   sendATCommand((char *)"AT+CGCONTRDP");
   sendATCommand((char *)"AT+CHTTPCREATE=\"http://iot.fvh.fi/\"");
   if (sendATCommand((char *)"AT+CHTTPCON=0")) {
@@ -74,13 +74,16 @@ void setupConnection() {
 
 uint8_t sendATCommand(char *ATCommand) {
   uint8_t aswerbytes[4] = {0, 0, 0, 0}; // Save last two chars + NL&CR to compare if ok
+  int16_t timeout=10000; // If no aswer in 10 sec timeout
   Serial.print("Sending AT command: ");
   //Send data to modem
   modem.print(ATCommand);
   modem.print("\r\n");
   //Wait for aswer
-  while (!modem.available()) {
+  while (!modem.available()) {  
     delay(50);
+    timeout=timeout-50;
+    if( timeout <= 0 ) return(2); // return 2 on timeout
   }
   // Copy aswer from modem to USB serial
   while (modem.available()) {
@@ -102,4 +105,14 @@ uint8_t sendATCommand(char *ATCommand) {
 void reboot() {
   Serial.println("Restarting...");
   ESP.restart();
+}
+
+void PowerOnModem() {
+  pollModem = 0;
+  Serial.println("Power on modem.");
+  digitalWrite( D1, LOW );
+  delay(500);
+  digitalWrite( D1,  HIGH );
+  delay(500);
+  pollModem = 1;
 }
